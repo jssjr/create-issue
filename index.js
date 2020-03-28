e778031580e76f8424497751b9106a4cf113eb4a
Scott Sanders <jssjr@github.com>
2020-03-28 09:53:17 -0400

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const tools = require('@actions/toolkit');
+
+async function run() {
+  try { 
+    const newIssue = await tools.github.issues.create({
+      ...tools.context.repo,
+      title: tools.inputs.title,
+      body: tools.inputs.body
+    })
+
+    core.setOutput('number', newIssue.number);
+    core.setOutput('id', newIssue.id);
+  } 
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
